a779388fe613ebb0fea0a5ba8fdfccb6fdaf7ecb
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-03 13:00:08 +0530

    Create CASTING.java

---
+++ CASTING.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        //casting 
+
+        //implicit casting
+
+        double price = 100.00;
+        double finalprice = price+18;
+
+        System.out.println(finalprice);
+
+        //explicit casting
+
+        int p = 200;
+        int fb = p + (int)36.152282;
+
+        System.out.println(fb);
+
+
+    }
+}
