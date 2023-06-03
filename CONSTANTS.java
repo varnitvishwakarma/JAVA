80df1aa87d1dff595d9e123c9852569477193793
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-03 13:05:43 +0530

    Create CONSTANTS.java

---
+++ CONSTANTS.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        // constants
+
+        int age=30;
+        age=31;
+        age=32;
+
+        final float PI=3.14F;
+        PI = 1.4F;
+        
+
+
+
+    }
+}
