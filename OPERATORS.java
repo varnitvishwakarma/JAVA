aa293478c8fdd9705779f60448eab00d5c909a34
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-03 14:15:02 +0530

    Create OPERATORS.java

---
+++ OPERATORS.java
@@ -0,0 +1,46 @@
+
+import java.util.Arrays;
+
+class HelloWorld {
+    public static void main(String[] args) {
+       // operators 
+
+       // arithmetic operations 
+
+       double a =5;
+       double b = 2;
+       double sum = a+b;
+       double mul= a*b;
+       double sub=a-b;
+       double div=a/b;
+       double modulo=a%b;
+       System.out.println(sum);
+       System.out.println(mul);
+       System.out.println(sub);
+       System.out.println(div);
+       System.out.println(modulo);
+      
+      // assignment operations 
+
+       int num=1;
+       System.out.println(num++);
+       System.out.println(num);
+       
+       int no=1;
+       System.out.println(++no);
+       System.out.println(no);
+
+       int number=1;
+       System.out.println(--number);
+       System.out.println(number);
+
+       int numbers=2;
+       System.out.println(numbers--);
+       System.out.println(numbers);
+
+      
+
+
+    }
+}
+
