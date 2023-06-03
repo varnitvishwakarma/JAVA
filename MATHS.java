b1dcc0738bdbaf51de152e1d852f9fbb4b68881f
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-03 14:30:56 +0530

    Create MATHS.java

---
+++ MATHS.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+class HelloWorld {
+    public static void main(String[] args) {
+       //Maths
+
+       //max
+
+       System.out.println(Math.max(5,8));
+
+       // min 
+
+       System.out.println(Math.min(5,9));
+       
+       //random 
+
+       System.out.println((int)(Math.random()*100));
+
+      
+
+
+    }
+}
