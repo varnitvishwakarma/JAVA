325b025f020c5e6911fdbb705a3508724288593c
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-04 14:08:17 +0530

    Create FUNCTION.java

---
+++ FUNCTION.java
@@ -0,0 +1,33 @@
+class HelloWorld {
+
+    public static void print(){
+        System.out.println("hello world !");
+    }
+
+    public static void name(String name ){
+        
+        System.out.println(name);
+        
+    }
+
+    public static void add (int a,int b){
+        int sum=a+b;
+        System.out.println(sum);
+
+    }
+    public static void main(String[] args) {
+        //FUNCTION
+
+       name("varnit");
+       name("varnit");
+       add(54,4);
+       print();
+
+
+           
+ }
+}
+
+    
+
+    
