3fae0272b4453cbabd71c08eba001a020f6eadcb
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-04 13:28:13 +0530

    Create BREAK AND CONTINUE.java

---
+++ BREAK AND CONTINUE.java
@@ -0,0 +1,27 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // BREAK AND CONTINUE 
+
+        int i =0;
+
+        while(true){
+            if(i==3){
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+            if(i>5){
+                break;
+            }
+        }
+
+
+    
+       
+ }
+}
+
+    
+
+    
