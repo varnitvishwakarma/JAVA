732ff21b07caef5ada5435997873d6c53474a827
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-04 13:46:21 +0530

    Create EXCEPTION HANDLING.java

---
+++ EXCEPTION HANDLING.java
@@ -0,0 +1,23 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // EXCEPTION HANDLING
+
+        int[] marks={98,87,89,97,78};
+        try{
+            System.out.println(marks[5]);
+        }catch(Exception exception){
+            System.out.println("EXCEPTION FOUND");
+        }
+         
+        System.out.println("I AM AN DATA SCIENTIST");
+
+
+
+    
+       
+ }
+}
+
+    
+
+    
