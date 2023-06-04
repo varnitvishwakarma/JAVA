7c2e762736c0211927805160c2febdec682add8c
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-06-04 12:54:55 +0530

    Create LOOPS.java

---
+++ LOOPS.java
@@ -0,0 +1,36 @@
+class HelloWorld {
+    public static void main(String[] args) {
+     //LOOPS
+
+     //FOR LOOP
+
+     for(int i=100;i>=1;i--){
+     System.out.println(i);
+    }
+
+     //WHILE LOOP
+
+     int j = 10;
+     while(j>=1){
+        System.out.println(j);
+        j--;
+     }
+
+     //DO WHILE LOOP
+
+     int k =100;
+     do{
+        System.out.println(k);
+        k--;
+     } while(k>=1);
+
+
+
+
+       
+ }
+}
+
+    
+
+    
